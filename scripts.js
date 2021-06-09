91fa503cbec86fd67391614a89b3cca5380fc3f0
James Kraus <jameskraus@Jamess-Air.attlocal.net>
2021-06-08 20:15:14 -0400

    action button opens prompt window and stores name as variable.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 const actionButton = document.getElementById("action-btn");
 
-actionButton.addEventListener("click",function openScheduleBox() {
-    console.log("TEST");
-})
+actionButton.addEventListener("click", openScheduleBox)
+
+function openScheduleBox() {
+    let name = prompt("What is your name?");
+}
+
+console.log(name)
